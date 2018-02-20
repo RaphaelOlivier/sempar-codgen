6de6bcc28bf6a5c09595f99785d0b890499bbcd6
Shirley Anugrah Hayati <shayati@Anthonys-MBP.wv.cc.cmu.edu>
2018-02-20 14:32:57 -0500

    Add Hearthstone dataset

--- checkpoint1/extract_data.py
+++ checkpoint1/extract_data.py
@@ -31,21 +31,21 @@ def main():
 	train_data, dev_data, test_data = deserialize_from_file("data/django.cleaned.dataset.freq5.par_info.refact.space_only.bin")
 
 	#uncomment below for Hearthstone data set
-	#train_data, dev_data, test_data = deserialize_from_file("hs.freq3.pre_suf.unary_closure.bin"
+	#train_data, dev_data, test_data = deserialize_from_file("hs.freq3.pre_suf.unary_closure.bin")
 
 	print("----- TRAIN -----")
 	train_length = len(train_data.examples)
-	print(train_length) #16000 instances
+	print(train_length) #16000 instances for django, 533 for hs
 	write_to_file("train_.txt", train_data, train_length)
 
 	print("----- DEV -----")
 	dev_length = len(dev_data.examples)
-	print(dev_length) #1000 instances
+	print(dev_length) #1000 instances for django, 66 for hs
 	write_to_file("dev_.txt", dev_data, dev_length)
 
 	print("----- TEST -----")
 	test_length = len(test_data.examples)
-	print(test_length) #1801 instances
+	print(test_length) #1801 instances, 66 for hs
 	write_to_file("test_.txt", test_data, test_length)
     
     #print(train_data.get_examples(0).query)

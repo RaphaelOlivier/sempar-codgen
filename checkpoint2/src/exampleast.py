a0a68ebf6ca64d17ecebd18f9c296a4dd9ade7c1
RaphaelOlivier <raphael.franck.olivier@gmail.com>
2018-03-08 18:43:01 -0500

    change token response form

--- checkpoint2/src/exampleast.py
+++ checkpoint2/src/exampleast.py
@@ -1,1 +1,6 @@
-sorted(my_list, reverse=True)
+class AssassinsBlade (WeaponCard):
+    def __init__(self):
+        super() . __init__(" Assassin's Blade ", 5, CHARACTER_CLASS . ROGUE, CARD_RARITY . COMMON)
+
+    def create_weapon(self, player):
+        return Weapon(3, 4)

d8b4f7ddc1532186f824b2e157e009f1da22e6e7
Shirley Anugrah Hayati <sweetpineappleforest@gmail.com>
2018-04-21 15:55:43 -0400

    simi score

--- finalproject/edit-distance/sentence_edit_distance.py
+++ finalproject/edit-distance/sentence_edit_distance.py
@@ -9,13 +9,13 @@ import os
 import sys
 import math
 
-def distance(raw_sentence, dict_sentence):
+def sentence_distance(first_sentence, second_sentence):
 	#print raw_word
 	#print dict_word
-	raw_sentence = raw_sentence.split(' ')
-	dict_sentence = dict_sentence.split(' ')
-	m = len(raw_sentence)+1
-	n = len(dict_sentence)+1
+	first_sentence = first_sentence.split(' ')
+	second_sentence = second_sentence.split(' ')
+	m = len(first_sentence)+1
+	n = len(second_sentence)+1
 	matrix = [[0]*(m) for _ in range(n)]
 	#print matrix
 	for i in range(n):
@@ -27,7 +27,7 @@ def distance(raw_sentence, dict_sentence):
 	#print "................."
 	for i in range(1,n):
 		for j in range(1,m):
-			if raw_sentence[j-1] == dict_sentence[i-1]:
+			if first_sentence[j-1] == second_sentence[i-1]:
 				penalty = 0
 			else:
 				penalty = 1
@@ -41,35 +41,51 @@ def Levenshtein_edit_distance(rawtext,dictionary,output):
 	raw = []
 	raw_ptr = open(rawtext)
 	dict_ptr = open(dictionary)
-	for raw_sentence in raw_ptr:
-		print (raw_sentence)
-		raw.append(raw_sentence)
-	for dict_sentence in dict_ptr:
-		print (dict_sentence)
-		vocabulary.append(dict_sentence)
+	for first_sentence in raw_ptr:
+		print (first_sentence)
+		raw.append(first_sentence)
+	for second_sentence in dict_ptr:
+		print (second_sentence)
+		vocabulary.append(second_sentence)
 
 	output_ptr = open(output, 'w')
 	i = 0
-	for raw_sentence in raw:
+	for first_sentence in raw:
 		min_distance = 99999;
-		for dict_sentence in vocabulary:
-			if(raw_sentence == dict_sentence ):
+		for second_sentence in vocabulary:
+			if(first_sentence == second_sentence ):
 				
 				#print "hello"
-				correct_sentence = dict_sentence
+				correct_sentence = second_sentence
 				min_distance = 0
 				continue;
 			else:
-				answer = distance(raw_sentence, dict_sentence)
+				answer = distance(first_sentence, second_sentence)
 				if(answer < min_distance):
 					min_distance = answer
-					correct_sentence = dict_sentence	
+					correct_sentence = second_sentence	
 		i = i+1
 		if( i == 500):
 			output_ptr.write(correct_sentence + " " + str(min_distance+1))
 		else:
 			output_ptr.write(correct_sentence + " " + str(min_distance+1)+"\n")		
-		
+
+x = "one two three four"
+x_m = "one four five"
+dist = sentence_distance(x, x_m)
+print dist
+
+#	x = first_sentence
+#	x_m = second_sentence
+def simi(first_sentence, second_sentence):
+	max_score = max(len(first_sentence.split()), len(second_sentence.split()))
+	simi = 1.0 - (sentence_distance(first_sentence, second_sentence)/max_score)
+	return simi
+
+simi_score = simi(x, x_m)
+print simi_score
+
+'''
 raw = sys.argv[1]
 dictionary = sys.argv[2]
 output = sys.argv[3]
@@ -78,4 +94,4 @@ Levenshtein_edit_distance(raw,dictionary,output)
 matrix = [[0]*(5) for _ in range(5)]
 for i in range(5):
 	matrix[i][0] = i
-
+'''
